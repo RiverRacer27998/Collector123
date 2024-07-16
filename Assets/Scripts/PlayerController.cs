a237724f6ef24e8c0f985b0d8e4e642a6e0174f8
RiverRacer27998 <sam1970sdn@gmail.com>
2024-07-16 20:00:05 +0300

    1.1

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontal;
+    private float vertical;
+
+    private int speed = 20;
+    private int rotationSpeed = 60;
+    private int force = 7
+
+    private bool onIsland;
+
+    public Rigidbody rb;
+
+    void Update()
+    {
+        horizontal = Input.GetAxis("Horizontal");
+        vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * vertical);
+        transform.Rotate(Vector3.up * Time.deltaTime * rotationSpeed * horizontal);
+
+        if (Input.GetMouseButtonDown((int)KeyCode.Space) && onIsland == true)
+        {
+            rb.AddForce(Vector3.up * force, ForceMode.Impulse);
+            onIsland = false;
+        }
+
+        private void
+    }
+}
